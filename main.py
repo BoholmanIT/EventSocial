1c65227f5edf5f970418eaea9305b72835ba29a0
BoholmanIT <boholman.it.tech.nstu@gmail.com>
2025-09-30 00:46:43 +0700

    create user_table

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from sqlalchemy.dialects.postgresql import ARRAY
+from typing import List
 
 DATABASE_URL = "postgresql+psycopg2://myuser:mypassword@localhost:5432/mydatabase"
 engine = create_engine(DATABASE_URL)
@@ -7,3 +9,14 @@ engine = create_engine(DATABASE_URL)
 class Base(DeclarativeBase):
     pass
 
+
+class User(Base):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    login: Mapped[str] = mapped_column(String(100), nullable=False)
+    password: Mapped[str] = mapped_column(String(20), nullable=False)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    fullname: Mapped[str | None] = mapped_column(String(100), nullable=True)
+    nickname: Mapped[str] = mapped_column(String(20))
+    
+    
